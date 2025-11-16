7870801189e94feccfb425444757f8f3cd513b0e
Rohit <rohitsharma813049@gmail.com>
2025-11-16 18:18:52 +0530

    update email

--- backend/utils/sendmail.js
+++ backend/utils/sendmail.js
@@ -4,14 +4,13 @@ require("dotenv").config();
 const sendmail=async({otp,email})=>{
     try{ 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  service: "gmail",
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
   }
-});
+})
+
 
   const mailOptions = {
     from : process.env.EMAIL_USER,
